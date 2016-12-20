901247f275ab6c84428c029c41cc1912377f1a74
Jared Edwards <jredwards519@gmail.com>
2016-12-19 21:16:27 -0500

    created the game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,8 @@
 console.log("JS file is connected to HTML! Woo!")
+
+
+// These are the four variables that represent the cards
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
